1625730a4e83cc988b5662ba8a009c73da4cc399
Shivangi Devgun <shivangi.devgun@neudesic.com>
2022-02-02 11:19:57 +0530

    added project, pipelines & tf script

---
+++ src/Database/Role.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace dotnet-core-hello-world
+{
+    public class Role
+    {
+        private Role()
+        {
+
+        }
+
+        public Role(string name, string description, bool isActive = true)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.IsActive = isActive;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool IsActive { get; private set; }
+    }
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_name = "Dev-RG"
+location="Central India"
